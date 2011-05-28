d8d3295c3f272f00f4aef945c7887d7434a332c6
Spencer Steffen <spencer@citrusme.com>
2011-05-27 19:25:05 -0700

    removed whitespace from main lib

--- lib/spree_essential_blog.rb
+++ lib/spree_essential_blog.rb
@@ -23,8 +23,7 @@ module SpreeEssentialBlog
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
-      end
-      
+      end      
     end
 
     config.to_prepare &method(:activate).to_proc
