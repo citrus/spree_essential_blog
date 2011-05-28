4ef76530afe0e89eba91905bc51ab26ad2a7170c
Spencer Steffen <spencer@citrusme.com>
2011-05-27 19:31:39 -0700

    prepping for disqus config

---
+++ lib/spree_essential_blog/custom_hooks.rb
@@ -0,0 +1,9 @@
+module SpreeEssentialBlog
+  
+  class CustomHooks < Spree::ThemeSupport::HookListener
+
+    #insert_after :admin_configurations_menu, 'admin/configurations/disqus_config'
+  
+  end
+  
+end
