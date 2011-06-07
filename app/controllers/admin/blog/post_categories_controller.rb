ed364cc0861c85c1f9af5fb9cc723b5eceae3ad2
Spencer Steffen <spencer@citrusme.com>
2011-06-07 16:23:01 -0700

    updated location after save and translated model name

--- app/controllers/admin/blog/post_categories_controller.rb
+++ app/controllers/admin/blog/post_categories_controller.rb
@@ -1,27 +1,17 @@
 class Admin::Blog::PostCategoriesController < Admin::ResourceController
   
   before_filter :load_data
-
+  
   def location_after_save
     admin_post_categories_url(@post)
   end
-
-  #update.after { redirect_to }
-  
-  # def edit
-  #   @category = PostCategory.find(params[:id])
-  # end
-  # 
-  # def update
-  #   @category = PostCategory.find(params[:id])
-  #   flash[:notice] = 'Category was successfully updated.' if @category.update_attributes(params[:post_category])
-  #   respond_to do |format|
-  #     format.html { redirect_to admin_posts_path }
-  #   end
-  # end
   
   private
-  
+    
+    def translated_object_name
+      I18n.t('post_category.model_name')
+    end
+      
     def load_data
       @post = Post.find_by_path(params[:post_id])
       @post_categories = PostCategory.all
