fc9611ce98294c6dd6ad368604bd09431fb1f03c
Spencer Steffen <spencer@citrusme.com>
2011-06-06 18:37:17 -0700

    working on tests

--- test/support/fixtures.rb
+++ test/support/fixtures.rb
@@ -1,12 +1,5 @@
 FactoryGirl.define do
 
-  factory :page do
-    title            "Just a page"
-    meta_title       { nav_title }
-    meta_description { "Nothing too cool here except the title: #{title}." } 
-    meta_keywords    { "just, something, in, a, list, #{title.downcase}" }
-  end
-
   factory :post do
     title     "Peanut Butter Jelly Time"
     path      { title.parameterize }
@@ -14,6 +7,10 @@ FactoryGirl.define do
     body      "Vivamus rutrum nunc non neque consectetur quis placerat neque lobortis. Nam vestibulum, arcu sodales feugiat consectetur, nisl orci bibendum elit, eu euismod magna sapien ut nibh. Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula egestas tincidunt. Nullam risus magna, ornare vitae varius eget, scelerisque a libero."
     tag_list  "peanut butter, jelly, sandwich, lunch"
     live      true
+  end
+  
+  factory :post_category do
+    name  "Jellies"
   end
 
 end
