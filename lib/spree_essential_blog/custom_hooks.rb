b37b44fb3b8048b71e12b9acbeaac11a9bb62653
Spencer Steffen <spencer@citrusme.com>
2011-07-06 09:54:12 -0700

    fixed partial paths for disqus config

--- lib/spree_essential_blog/custom_hooks.rb
+++ lib/spree_essential_blog/custom_hooks.rb
@@ -2,7 +2,7 @@ module SpreeEssentialBlog
   
   class CustomHooks < Spree::ThemeSupport::HookListener
 
-    insert_after :admin_configurations_menu, 'admin/configurations/disqus_config'
+    insert_after :admin_configurations_menu, 'blog/admin/configurations/disqus_config'
   
   end
   
