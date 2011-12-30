ca2554af71a3bac34ec192dba5c96977f95898ef
Spencer Steffen <spencer@citrusme.com>
2011-12-30 11:54:05 -0800

    fix decorator load path in main lib

--- lib/spree_essential_blog.rb
+++ lib/spree_essential_blog.rb
@@ -17,7 +17,7 @@ module SpreeEssentialBlog
 
     config.to_prepare do
       #loads application's model / class decorators
-      Dir.glob File.expand_path("../../app/**/*_decorator*.rb") do |c|
+      Dir.glob File.expand_path("../../app/**/*_decorator.rb", __FILE__) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
 
