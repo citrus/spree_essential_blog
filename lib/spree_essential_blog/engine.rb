a11a8e3a214254dcf432d44273e3c7a9a5891715
Spencer Steffen <spencer@citrusme.com>
2012-05-07 17:14:02 -0700

    fix disqus comments and blog config

--- lib/spree_essential_blog/engine.rb
+++ lib/spree_essential_blog/engine.rb
@@ -6,8 +6,9 @@ module SpreeEssentialBlog
     config.autoload_paths += %W(#{config.root}/lib)
 
     config.to_prepare do
-    
-      SpreeEssentialBlog::Config = Spree::BlogConfiguration.new unless SpreeEssentialBlog.const_defined?(:Config)
+      
+      # Would be nice to use const_defined?(:Config, false) here but it's not 1.8.x compatible
+      ::SpreeEssentialBlog::Config = Spree::BlogConfiguration.new unless SpreeEssentialBlog.constants.include?(:Config)
       
       Dir.glob File.expand_path("../../../app/**/*_decorator.rb", __FILE__) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
